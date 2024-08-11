6d9a7e1d7e075f626d8fb03e7cdafbb4f8085db8
patryk3211 <patrykmierzy@gmail.com>
2024-08-11 18:06:38 +0200

    Update

--- makefiles/styletest_linux/main.c
+++ makefiles/styletest_linux/main.c
@@ -7,20 +7,14 @@
 #include "src/modules/system_menu.h"
 #include "src/modules/lvgl/lvgl_support.h"
 
-#include <stdio.h>
-
-// lv_indev_t *mouse;
-lv_indev_t *mousewheel;
-lv_indev_t *keyboard;
-
 int main()
 {
 	lvgl_support_init();
 	
 	lv_display_t *display = lv_sdl_window_create(480, 320);
 	lv_indev_t *mouse = lv_sdl_mouse_create();
-	mousewheel = lv_sdl_mousewheel_create();
-	keyboard = lv_sdl_keyboard_create();
+	lv_indev_t *mousewheel = lv_sdl_mousewheel_create();
+	lv_indev_t *keyboard = lv_sdl_keyboard_create();
 
 	lvgl_use_display(display);
 	LVGL_ADD_INDEV(mouse);
