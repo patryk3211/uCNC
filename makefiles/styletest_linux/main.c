4cfd51a78ab3a197cc2da813f0d64fbf1ef3b0e0
patryk3211 <patrykmierzy@gmail.com>
2024-08-22 16:04:46 +0200

    Update testing

--- makefiles/styletest_linux/main.c
+++ makefiles/styletest_linux/main.c
@@ -3,25 +3,27 @@
 #include "drivers/sdl/lv_sdl_mouse.h"
 #include "drivers/sdl/lv_sdl_keyboard.h"
 
+#include "misc/lv_types.h"
 #include "src/modules/lvgl/style/style.h"
 #include "src/modules/system_menu.h"
 #include "src/modules/lvgl/lvgl_support.h"
+
+lv_display_t *lvgl_create_display()
+{
+	return lv_sdl_window_create(480, 320);
+}
 
 int main()
 {
 	lvgl_support_init();
 	
-	lv_display_t *display = lv_sdl_window_create(480, 320);
 	lv_indev_t *mouse = lv_sdl_mouse_create();
 	lv_indev_t *mousewheel = lv_sdl_mousewheel_create();
 	lv_indev_t *keyboard = lv_sdl_keyboard_create();
 
-	lvgl_use_display(display);
 	LVGL_ADD_INDEV(mouse);
 	LVGL_ADD_INDEV(mousewheel);
 	LVGL_ADD_INDEV(keyboard);
-
-	lvgl_support_end_init();
 
 	EVENT_INVOKE(cnc_reset, 0);
 
